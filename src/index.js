7342f6d994905610f64820213845ab4c61f30107
DANOverkill <danielz.nas@gmail.com>
2026-07-31 10:40:28 -0300

    createProject function. Expose createProject and Project to window for testing

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import "./style.css";
 import Project from "./project.js";
 
 console.log("Webpack template running!");
+
+
+const createProject = (input) => {
+    
+    const newProjectName = Project(input);
+    return newProjectName;
+    
+};
+
+
+//exposure for testing
+window.createProject = createProject;
+window.Project = Project;
